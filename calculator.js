e1e6eb31d57fc887d715a169e8afb9b52da3398f
Gabenley Bien Aime <gabenley1@gmail.com>
2022-12-20 21:46:47 -0300

    Final Change

--- calculator.js
+++ calculator.js
@@ -11,6 +11,21 @@ const myfun = (req, res) => {
     // console.log(__dirname)
 }
 
+const myfun2 = (req, res) => {
+    res.sendFile(__dirname + "/bmicalculator.html");
+      // console.log(__dirname)
+}
+
+const myfun3 = (req, res) => {
+     let w = parseFloat(req.body.w);
+     let h = parseFloat(req.body.h);
+
+     let re = w / (h * h)
+      console.log(req.body)
+      res.send("Result is " + re)
+    //   res.redirect("/");
+}
+
 const calculate = (req, res) => {
     console.log()
     let num1 = Number(req.body.n1);
@@ -22,10 +37,18 @@ const calculate = (req, res) => {
     res.send(text)
 }
 
+const home = (req, res) => {
+    let name = "Gabenley";
+    // res.sendFile(__dirname + "/view/home.pug", {name:name});
+    res.render('home.pug', { name: 'John Doe', age: 21 });
+}
+
 
 
 app.get('/', myfun)
-
+app.get('/bmi', myfun2)
+app.get('/home', home)
+app.post('/bmi', myfun3)
 app.post('/calculate',calculate)
 
 app.listen(port, (req, res) => {
